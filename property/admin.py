bd38449b8e589c06921afa83f40ed9d3971269df
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-03-16 21:17:16 +0500

    Добавлена модель Owner для хранения данных о собственниках

--- property/admin.py
+++ property/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Owner
 
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
     search_fields = ['town', 'address', 'owner']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     list_editable = ['new_building']
     raw_id_fields = ('liked_by', )
@@ -18,3 +18,10 @@ class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('flat', 'user')
     list_display = ('user', 'flat', 'created_at')
     search_fields = ('user__username', 'flat__address', 'text')
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flats',)
+    list_display = ('full_name', 'phonenumber', 'pure_phone')
+    search_fields = ('full_name', 'phonenumber', 'pure_phone')
--- property/models.py
+++ property/models.py
@@ -93,3 +93,24 @@ class Complaint(models.Model):
 
     def __str__(self):
         return f'Жалоба от {self.user.username} ({self.flat.address})'
+
+
+class Owner(models.Model):
+    full_name = models.CharField('ФИО владельца', max_length=200)
+    phonenumber = models.CharField('Номер владельца', max_length=20)
+    pure_phone = PhoneNumberField(
+        'Нормализованный номер',
+        blank=True,
+        null=True,
+        region='RU',
+        help_text='Формат: +7XXXXXXXXXX'
+    )
+    flats = models.ManyToManyField(
+        Flat,
+        verbose_name='Квартиры в собственности',
+        related_name='owners',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.full_name
